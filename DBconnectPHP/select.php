f8946e15eecc83b1bd01386321ef158d18bb2025
my_name <77881067+TomoyaOkuhiro@users.noreply.github.com>
2022-05-08 16:24:48 +0900

    COUNT
    COUNT,PHP

--- DBconnectPHP/select.php
+++ DBconnectPHP/select.php
@@ -11,10 +11,10 @@
 <body>
   <?php
   $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
-  $records = $db->query('select * from my_items');
+  $records = $db->query('select count(*) as cnt from my_items');
   if ($records) {
     while ($record = $records->fetch_assoc()) {
-      echo $record['item_name'] . ',' . $record['price'];
+      echo $record['count(*)'] . ',' . $record['price'];
     }
   } else {
     echo $db->error;
