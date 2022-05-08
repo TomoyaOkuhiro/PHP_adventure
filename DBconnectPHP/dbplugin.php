4c80fdd741eed8124db3e12b4ca633838d91d849
my_name <77881067+TomoyaOkuhiro@users.noreply.github.com>
2022-05-08 15:26:54 +0900

    DB<--->PHP SELECT
    SELECT

--- DBconnectPHP/dbplugin.php
+++ DBconnectPHP/dbplugin.php
@@ -12,7 +12,18 @@
   <?php
   $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
   echo '接続が正常に完了しました。';
+  $db->query('drop table if exists test');
+  $success = $db->query('create table test(id INT)');
+  if ($success) {
+    echo 'テーブルを削除して、作成されました';
+  } else {
+    echo 'SQLが正常に動作しませんでした。';
+  }
+
   ?>
+
+  <!-- if exists 存在するなら -->
+
 </body>
 
 </html>
