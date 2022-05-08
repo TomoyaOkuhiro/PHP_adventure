e60b48e350a0a2e0b6e727f688f3ee6029e2153e
my_name <77881067+TomoyaOkuhiro@users.noreply.github.com>
2022-05-08 10:48:09 +0900

    relation
    table1 ----------- table2

---
+++ SQL/relation.sql
@@ -0,0 +1,12 @@
+table1 ========= table2
+
+SELECT * FROM table1,table2
+
+, を使ってテーブルを複数記述する場合は WHERE が必要
+
+select * from makers, my_items where makers.id = my_items.maker_id;
+
+>shortcut
+
+select * from makers m, my_items i where m.id = i_id;
+
