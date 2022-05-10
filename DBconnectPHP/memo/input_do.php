47bcf38978a1e14ee785d667294fd0c75ec2dabf
my_name <77881067+TomoyaOkuhiro@users.noreply.github.com>
2022-05-10 22:58:45 +0900

    require
    require file connecting

--- DBconnectPHP/memo/input_do.php
+++ DBconnectPHP/memo/input_do.php
@@ -1,6 +1,6 @@
 <?php
 $memo = filter_input(INPUT_POST, 'memo', FILTER_SANITIZE_SPECIAL_CHARS);
-$db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
+require('dbconnect.php');
 $stmt = $db->prepare('insert into memos(memo) values(?)');
 if (!$stmt) :
   die($db->error);
