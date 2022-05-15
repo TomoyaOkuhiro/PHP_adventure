d08e508f3d645cbbd85016135d948763b512123d
my_name <77881067+TomoyaOkuhiro@users.noreply.github.com>
2022-05-15 23:39:50 +0900

    update_page
    updatepage create

---
+++ memo/update.php
@@ -0,0 +1,34 @@
+<?php
+require('dbconnect.php');
+$db->prepare('select * from memos where id=?');
+if (!$stmt) {
+  die($db->error);
+}
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$stmt->bind_param('i', $id);
+$stmt->execute();
+
+$stmt->bind_result($id, $memo, $created);
+$stmt->fetch();
+if (!$result)
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>メモの編集</title>
+</head>
+
+<body>
+  <form action="input_do.php" method="post">
+    <textarea name="memo" cols="50" rows="10" placeholder="メモを入力してください">
+      <?php echo htmlspecialchars($memo); ?>
+    </textarea>
+    <button type="submit">編集する</button>
+  </form>
+</body>
+
+</html>
