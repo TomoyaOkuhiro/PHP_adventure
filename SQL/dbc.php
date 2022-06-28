eeca401a682e16fa7e4911f0e57a99cfa22663ae
my_name <77881067+TomoyaOkuhiro@users.noreply.github.com>
2022-06-28 22:42:46 +0900

    function
    functionを用いてリファクタリング

--- SQL/dbc.php
+++ SQL/dbc.php
@@ -1,29 +1,68 @@
 <?php
+//  関数一つに一つの機能のみを持たせる
+//  1.DB接続
+//  2.データを取得する.
+//  3.カテゴリー名を表示する.
 
-$dsn = 'mysql:host=localhost;dbname=blog_app;charset=utf8';
-$user = 'bloguser';
-$pass = 'blogpass';
+// 1.データベース接続
+// 引数なし
+// 返り値:接億結果を返す
 
-try {
-  $dbh = new PDO($dsn, $user, $pass, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-  ]);
-  // echo '接続成功';
-  // 1. SQLの準備
+
+function dbConnect()
+{
+
+  $dsn = 'mysql:host=localhost;dbname=blog_app;charset=utf8';
+  $user = 'bloguser';
+  $pass = 'blogpass';
+
+  try {
+    $dbh = new PDO($dsn, $user, $pass, [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
+  } catch (PDOException $e) {
+    echo '接続失敗' . $e->getMessage();
+    exit();
+  };
+
+  return $dbh;
+}
+
+//  2.データを取得する
+// 引数:なし
+// 返り値:取得したデータ
+function getAllBlog()
+{
+  $dbh = dbConnect();
+  //  SQLの準備
   $sql = 'SELECT * FROM blog';
   // 2. SQLの実行
   $stmt = $dbh->query($sql);
 
   // 3. SQLの実行結果を受け取る
   $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  var_dump($result);
+  return $result;
   $dbh = null;
-} catch (PDOException $e) {
-  echo '接続失敗' . $e->getMessage();
-  exit();
-};
+}
+// 取得したデータを表示
+$blogData = getAllblog();
 
+// 3.カテゴリー名を表示
+// 引数:int
+// 返り値:カテゴリの文字列
+
+function setCategoryName($category)
+{
+
+  if ($category === '1') {
+    return 'ブログ';
+  } elseif ($category === 2) {
+    return '日常';
+  } else {
+    return 'その他';
+  }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -42,7 +81,7 @@ try {
       <th>タイトル</th>
       <th>カテゴリ</th>
     </tr>
-    <?php foreach ($result as $column) : ?>
+    <?php foreach ($blogData as $column) : ?>
       <tr>
         <td><?php echo $column['id'] ?></td>
         <td><?php echo $column['title'] ?></td>
